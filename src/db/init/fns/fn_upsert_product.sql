53f9591a897c9449c8304149c34c06e2cd325ac1
Simon Stipcich <simon.stipcich@derivco.co.za>
2023-11-09 09:12:18 +0200

    fix:seed script breaking auto increment of prime key in product.

--- src/db/init/fns/fn_upsert_product.sql
+++ src/db/init/fns/fn_upsert_product.sql
@@ -11,25 +11,34 @@ as $$
 declare v_id bigint;
 begin
 
+    RAISE NOTICE 'p_id: %', p_id;
+    RAISE NOTICE 'p_usr_id: %', p_usr_id;
+    RAISE NOTICE 'p_description: %', p_description;
+    RAISE NOTICE 'p_code: %', p_code;
+
     IF p_id > 0 THEN
+
       update tb_product
       set 
         description = p_description,
         code = p_code
       where id = p_id
       returning id into v_id;
+
     ELSE
+
       insert into tb_product
       (
-          description, 
-          code
+        description, 
+        code
       ) 
       values 
       (
-          p_description, 
-          p_code
+        p_description, 
+        p_code
       ) 
       returning id into v_id;
+
     END IF;
 
     return v_id;
--- src/db/init/seed/insert-tb_lst_status.sql
+++ src/db/init/seed/insert-tb_lst_status.sql
@@ -1,2 +1,2 @@
-insert into tb_lst_status(id, usr_id, description) values (1, 0, 'Active');
-insert into tb_lst_status(id, usr_id, description) values (2, 0, 'Deleted');
+insert into tb_lst_status(usr_id, description) values (0, 'Active');
+insert into tb_lst_status(usr_id, description) values (0, 'Deleted');
